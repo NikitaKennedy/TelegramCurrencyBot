fa1f7c9b574490f3def068f03fb5e91df6f9983f
NikitaKennedy <123498494+NikitaKennedy@users.noreply.github.com>
2023-03-27 23:22:50 +0300

    Add files via upload

---
+++ GetPrise.py
@@ -0,0 +1,53 @@
+import requests
+import json
+import config
+from extensions import APIException
+
+
+class Converter:
+    @staticmethod
+    # Функция принимает значения валюты, конвертируемой валюты и количество валюты
+    def get_price(base: str, quote: str, amount: str):
+
+        if base == quote:
+            raise APIException("Нельзя переводить валюту саму в себя")
+
+        try:
+            base = config.CURRENCY_CONVERSION[base]
+        except KeyError:
+            raise APIException(f"Валюта {base} не поддерживается")
+
+        try:
+            quote = config.CURRENCY_CONVERSION[quote]
+        except KeyError:
+            raise APIException(f"Валюта {quote} не поддерживается")
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException("Количество должно быть числом")
+
+        if amount <= 0:
+            raise APIException("Количество должно быть больше нуля")
+        # Выполняем запрос API
+        url = f"https://api.apilayer.com/currency_data/convert?to={quote}&from={base}&amount={amount}"
+        headers = {"apikey": f"{config.API_KEY}"}
+        payload = {}
+        # Получаем ответ в формате json
+        response = requests.get(url, headers=headers, data=payload)
+        response_json = json.loads(response.content.decode('utf-8'))
+
+        try:
+            #Получаем цену и стоимость запрашиваемой валюты
+            price = float(response_json["info"]["quote"])
+            result = price * amount
+            return round(result, 1,)
+        except (KeyError, ValueError):
+            raise APIException("Ошибка получения курса валют")
+
+
+    @staticmethod
+    #Возвращаем ответ в виде списка со стоимостью запрашиваемой валюты
+    def get_available_currencies():
+        return list(config.CURRENCY_CONVERSION.keys())
+
---
+++ config.py
@@ -0,0 +1,10 @@
+# Тоукен бота
+TOKEN = '6293506443:AAE50mm3Zw4cEEL4mukIRy1euV5sHUYwVpM'
+# API_KEY полученный при регистрации на apilayer.com
+API_KEY = 'OUKT1J4k9QbylfoHGDkO6jIs57wPkHNB'
+# словарь с доступными валютами, можно добавлять новые в соответсвии со значениями международных  валютных кодов
+CURRENCY_CONVERSION = {
+    'USD': 'usd',
+    'EUR': 'eur',
+    'RUB': 'rub',
+}
